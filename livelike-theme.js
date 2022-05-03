37cae13efe3340e10d7dbb10cfd2c5a2f1813d3e
xhevo <xhevat@livelike.com>
2022-05-03 19:49:49 +0200

    updated style

--- livelike-theme.js
+++ livelike-theme.js
@@ -1,21 +1,66 @@
+const globalWidgetStyle = {
+    header: {
+        fontColor: "000000",
+        borderRadius: [25, 25, 0, 0],
+        background: {
+            color: "FF0000",
+            format: "fill"
+        },
+        margin: [25, 0, 0, 0],
+    },
+    body: {
+        fontColor: "000000",
+        background: {
+            color: "cccccc",
+            format: "fill"
+        },
+        margin: [0, 0, 100, 0],
+        borderRadius: [0, 0, 25, 25]
+    },
+    selectedOption: {
+        borderColor: "000000",
+        background: {
+            color: "aaaaaa",
+            format: "fill"
+        },
+    },
+    correctOptionBar: {
+        background: {
+            colors: ["00000088", "ffffff"],
+            direction: 0,
+            format: "uniformGradient"
+        },
+    },
+    incorrectOptionBar: {
+        background: {
+            colors: ["00000088", "ffffff"],
+            direction: 0,
+            format: "uniformGradient"
+        },
+    },
+    unselectedOptionBar: {
+        background: {
+            colors: ["00000088", "ffffff"],
+            direction: 0,
+            format: "uniformGradient"
+        },
+    },
+    selectedOptionBar: {
+        background: {
+            colors: ["00000088", "ffffff"],
+            direction: 0,
+            format: "uniformGradient"
+        },
+    }
+};
+
 const setupTheme = () => {
     LiveLike.applyTheme({
         widgets: {
-            prediction: {
-                header: {
-                    background: { format: 'fill', color: '#FF0000' }
-                }
-            },
-            poll: {
-                header: {
-                    background: { format: 'fill', color: '#FF0000' }
-                }
-            },
-            quiz: {
-                header: {
-                    background: { format: 'fill', color: '#FF0000' }
-                }
-            }
+            poll: globalWidgetStyle,
+            quiz: globalWidgetStyle,
+            prediction: globalWidgetStyle,
+            imageSlider: globalWidgetStyle
         }
     });
 };
