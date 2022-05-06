9ecd29a622f4077866c53a747f2c1670bfb3fb71
xhevo <xhevat@livelike.com>
2022-05-06 14:19:19 +0200

    updated ui

--- livelike-theme.js
+++ livelike-theme.js
@@ -84,7 +84,94 @@ const setupTheme = () => {
         widgets: {
             poll: globalWidgetStyle,
             quiz: globalWidgetStyle,
-            prediction: globalWidgetStyle,
+            prediction: {
+                header: {
+                    borderColor: "00aaff",
+                    borderWidth: 1,
+                    fontColor: "ffffff",
+                    background: {
+                        color: "00aaff",
+                        format: "fill"
+                    },
+                    margin: [10, 0, 0, 0],
+                },
+                body: {
+                    borderColor: "00aaff",
+                    borderWidth: 1,
+                    fontColor: "000000",
+                    background: {
+                        color: "dedede",
+                        format: "fill"
+                    },
+                    margin: [0, 0, 10, 0]
+                },
+                footer: {
+                    background: {
+                        color: "000000",
+                        format: "fill"
+                    }
+                },
+                selectedOption: {
+                    borderColor: "00aaff",
+                    borderWidth: 1,
+                    background: {
+                        color: "ffffff",
+                        format: "fill"
+                    },
+                },
+                correctOptionBar: {
+                    borderColor: "00aaff",
+                    borderWidth: 1,
+                    background: {
+                        colors: "dedede",
+                        format: "fill"
+                    },
+                },
+                incorrectOptionBar: {
+                    borderColor: "00aaff",
+                    borderWidth: 1,
+                    background: {
+                        colors: "dedede",
+                        format: "fill"
+                    },
+                },
+                unselectedOptionBar: {
+                    borderColor: "00aaff",
+                    borderWidth: 1,
+                    background: {
+                        colors: "dedede",
+                        format: "fill"
+                    },
+                },
+                selectedOptionBar: {
+                    borderColor: "ff0000",
+                    borderWidth: 1,
+                    background: {
+                        colors: "dedede",
+                        format: "fill"
+                    },
+                },
+                interactiveTrackLeft: {
+                    background: {
+                        color: "ffffff",
+                        format: "fill"
+                    },
+                },
+                interactiveTrackRight: {
+                    background: {
+                        color: "44bbff",
+                        format: "fill"
+                    },
+                },
+                selectedOptionPercentage: {
+                    borderColor: "00000000",
+                    background: {
+                      color: "00000000",
+                      format: "fill"
+                    },
+                    fontColor: "00000000",
+                  },
+            },
             imageSlider: globalWidgetStyle,
             cheerMeter: globalWidgetStyle,
             alert: globalWidgetStyle,
